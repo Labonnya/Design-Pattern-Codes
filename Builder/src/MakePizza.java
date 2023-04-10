3a6063dfef17a78c7ca4706e995aee96970d17ee
Labonnya <75426550+Labonnya@users.noreply.github.com>
2023-04-10 16:53:59 +0600

    Delete Builder directory

--- Builder/src/MakePizza.java
+++
@@ -1,8 +0,0 @@
-public interface MakePizza {
-    public void choosePizzaDough();
-    public void choosePizzaMeat();
-    public void choosePizzaSauce();
-    public void choosePizzaToppings();
-
-    public Pizza getPizza();
-}
